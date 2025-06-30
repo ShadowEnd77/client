86ca7b447e2c0a9a4f214f730a65a34396cf82b1
tikhomirovofficial <arteom.tixomirow@yandex.ru>
2025-07-01 00:03:49 +0300

    motion fix game dialog card

--- src/features/game/ui/scenes/GameSceneCard/index.tsx
+++ src/features/game/ui/scenes/GameSceneCard/index.tsx
@@ -3,7 +3,6 @@ import styles from './gameSceneCard.module.scss'
 import { GameDialog } from '../../../../../types/entities'
 import previewImage from '../../../../assets/images/preview.jpg'
 import { motion } from "motion/react"
-import { ip } from "../../../../../api/instance";
 
 export type GameSceneCard = {
   delayShow: number
